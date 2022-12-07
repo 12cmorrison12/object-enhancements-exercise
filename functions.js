2902a62ff525eef0a014b1cf75f032d4799f0ff2
unknown <12cmorrison12@gmail.com>
2022-12-06 19:11:19 -0700

    first commit

---
+++ functions.js
@@ -0,0 +1,19 @@
+// Same keys and values
+
+function createInstructor(firstName, lastName){
+  return {
+    firstName: firstName,
+    lastName: lastName
+  }
+}
+
+// Same keys and values ES2015
+/* Write an ES2015 Version */
+
+function createInstructor(firstName, lastName) {
+  return {
+    firstName,
+    lastName
+  }
+};
+
